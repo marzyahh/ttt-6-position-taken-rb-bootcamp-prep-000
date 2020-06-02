ea87ba25be11031eecc16da4211d3769f9408c0e
marzyah <marzyahhooseini74@gmail.com>
2020-06-02 16:13:28 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,5 +5,6 @@ if index[0] == " " &&  index[1] == " " &&  index[2] == " " &&
   index[3] == " " &&  index[4] == " " &&  index[5] == " " &&  
   index[6] == " " &&  index[7] == " " &&  index[8] == " "
     position_taken?(board, index) == false
+    elsif 
 end
 end

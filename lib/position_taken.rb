b2c2d2cd0a3151cd8d6b8d37bf639f10c5ae6dca
marzyah <marzyahhooseini74@gmail.com>
2020-06-02 16:09:53 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+def position_taken?(board , index)
+  board = ["", "", "", "", "", "", "", "", ""]
+if index[0] == " " &&  index[1] == " " &&  index[2] == " " && 
+  index[3] == " " &&  index[4] == " " &&  index[5] == " " &&  
+  index[6] == " " &&  index[7] == " " &&  index[8] == " "
+  puts index[] = false
+end
+end
